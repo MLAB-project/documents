c15cf643039191db26232384cede104a3369d3c1
Roman Dvořák <romandvorak@mlab.cz>
2022-04-06 11:40:25 +0200

    pridani skriput

---
+++ copy_workflow_to_repo.sh
@@ -0,0 +1,8 @@
+
+#  zkopiruje workflow soubor do nadrazeneho repozitare
+#  Predpoklada se, ze tento repozitar je jako submodul
+#  a je umisten v `/doc/assets`
+#
+
+mkdir -p ../../.github/workflows
+cp workflows/kicad_outputs.yml ../../.github/workflows/kicad_outputs.yml
