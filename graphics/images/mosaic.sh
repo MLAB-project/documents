d382b644b9a0f6c36275e736c8b9dd9655a38dc6
Jakub Kakona <kaklik@mlab.cz>
2022-12-13 22:01:38 +0100

    uniform edge ratio of all images.

--- graphics/images/mosaic.sh
+++ graphics/images/mosaic.sh
@@ -1,4 +1,4 @@
 
 #https://stackoverflow.com/questions/20737061/merge-images-side-by-sidehorizontally
 
-montage ACOUNTER01A.jpg DIVISEK01.png Li-ion-Charger.jpg RSMS01B.JPG -tile 2x2 -geometry +0+0 MLAB_mosaic.jpg
+montage ACOUNTER01A.jpg RMDS02E.jpg Li-ion-Charger.jpg RSMS01B.JPG -tile 2x2 -geometry +0+0 MLAB_mosaic.jpg
