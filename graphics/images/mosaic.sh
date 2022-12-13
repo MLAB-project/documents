11c043ef84a2d1b12a15261c4d7c8ab69892dee5
Jakub Kákona <kaklik@mlab.cz>
2022-12-13 14:32:12 +0100

    Add a script to mosaic graphics.

---
+++ graphics/images/mosaic.sh
@@ -0,0 +1,4 @@
+
+#https://stackoverflow.com/questions/20737061/merge-images-side-by-sidehorizontally
+
+montage figur_peaks_1.png figur_peaks_2.png figur_peaks_3.png figur_peaks_4.png -tile 1x4 -geometry +0+0 camera_lightning_duration.png
