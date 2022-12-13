11ec8977f04f73390f131005a266f5c43e612901
Jakub Kakona <kaklik@mlab.cz>
2022-12-13 21:54:50 +0100

    The first attempt to make MLAB mosaic.

--- graphics/images/mosaic.sh
+++ graphics/images/mosaic.sh
@@ -1,4 +1,4 @@
 
 #https://stackoverflow.com/questions/20737061/merge-images-side-by-sidehorizontally
 
-montage figur_peaks_1.png figur_peaks_2.png figur_peaks_3.png figur_peaks_4.png -tile 1x4 -geometry +0+0 camera_lightning_duration.png
+montage ACOUNTER01A.jpg DIVISEK01.png Li-ion-Charger.jpg RSMS01B.JPG -tile 2x2 -geometry +0+0 MLAB_mosaic.jpg
