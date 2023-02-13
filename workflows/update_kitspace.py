1016f8aa4778705edeec87d899214670371c5acd
Jakub Kákona <kaklik@mlab.cz>
2023-02-13 23:47:54 +0100

    The first version of kitspace metadata generator.

---
+++ workflows/update_kitspace.py
@@ -0,0 +1,51 @@
+
+# This tool should update kitspace.yaml metadata file.
+# It should be run from the repository root.
+
+from github import Github
+import yaml
+from yaml import load, dump
+from yaml import CLoader as Loader, CDumper as Dumper
+import os
+from glob import glob
+
+g = Github(os.environ['gh_token'])
+repo = g.get_repo(os.environ['gh_repository'])
+print(repo)
+
+config_file = 'kitspace.yaml'
+new = 0
+
+try:
+    stream = open(config_file, 'r')
+    data = yaml.full_load(stream)
+    stream.close()
+except:
+    print("File does not exists")
+    data = None
+
+stream = open(config_file, 'w+')
+print(data, type(data))
+
+if data:
+    new = 0
+else:
+    data = {}
+    data['homepage'] = False
+    data['mark'] = 50
+    new = 1
+
+if new:
+    pass
+
+data['summary'] = repo.description
+data['site'] = os.environ.get('gh_url', "https://www.github.com/MLAB-project")+os.environ.get('gh_repo', "repository_name")
+#data['color'] = "white"
+data['bom'] = 
+data['gerbers'] = 'hw/cam_profi/'
+data['eda']['type'] = 'kicad'
+data['eda']['pcb'] = 'hw/sch_pcb/'++os.environ.get('gh_repo', "repository_name")+'.kicad_pcb'
+    
+print(data)
+yaml.dump(data, stream)
+stream.close()
