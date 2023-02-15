51396a7477e3b872b8eab9337ccf4ced9d1d846b
Jakub Kákona <kaklik@mlab.cz>
2023-02-15 12:50:56 +0100

    Update update_kitspace.py
    Change BOM to use kicad_pcb.

--- workflows/update_kitspace.py
+++ workflows/update_kitspace.py
@@ -39,7 +39,7 @@ if new:
 data['summary'] = repo.description
 data['site'] = os.environ.get('gh_url', "https://www.github.com/MLAB-project")
 #data['color'] = "white"
-data['bom'] = ''
+data['bom'] = 'hw/sch_pcb/'+os.environ.get('gh_repo', "repository_name")+'.kicad_pcb'
 data['gerbers'] = 'hw/cam_profi/'
 data['eda'] = {}
 data['eda']['type'] = 'kicad'
