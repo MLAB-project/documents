f1de387f6ca920740154e94da90a91b1d6bce7ad
Jakub Kákona <kaklik@mlab.cz>
2023-09-10 14:49:01 +0200

    Update copy_workflow_to_repo.sh
    Improve the clarity of the script messages.

--- workflows/copy_workflow_to_repo.sh
+++ workflows/copy_workflow_to_repo.sh
@@ -8,11 +8,11 @@ mkdir -p ../../../.github/workflows
 
 if [ "$(find ../../../hw -name '*kicad_pro'  2>/dev/null)" ]
 then
-	echo "This is KICAD project"
+	echo "KiCAD project is detected"
 	cp kicad_outputs.yml ../../../.github/workflows/kicad_outputs.yml
 else
    rm ../../../.github/workflows/kicad_outputs.yml 2>/dev/null || true
-   echo "Kicad not found"
+   echo "KiCAD project not found"
 fi
 cp update_actions.yml ../../../.github/workflows/update_actions.yml
 cp metadata_updater.yml ../../../.github/workflows/metadata_updater.yml
